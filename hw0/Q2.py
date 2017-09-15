a23c5c89771d9c1e8bbaec9bdc22a1c74c348b8e
Eric Lee <ericlee@Erics-MacBook-Pro.local>
2017-09-15 14:10:07 +0800

    Change output from jpg to png

--- hw0/Q2.py
+++ hw0/Q2.py
@@ -16,16 +16,7 @@ def main():
 			r, g, b = pixels[i, j]
 			pixels[i, j] = (int(r/2), int(g/2), int(b/2))
 
-	img.save("Q2.jpg", format='PNG')
-
-	'''
-	im2 = Image.open("Q2.jpg")
-	pixels2 = im2.load()
-	for i in range(img.size[0]):
-		for j in range(img.size[1]):
-			print(str(pixels[i, j]) + " " + str(pixels2[i, j]))
-			input("")
-	'''
+	img.save("Q2.png", format='PNG')
 
 if __name__ == '__main__':
 	main()
