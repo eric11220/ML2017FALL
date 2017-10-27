4368a3f85e43b5b864343b643d039dbe1c166a90
Eric Lee <ericlee@Erics-MacBook-Pro.local>
2017-10-27 19:44:24 +0800

    Finished

---
+++ hw2/hw2_best.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 dataProcessing.py $2 data/my_test test
+python3 nn/test.py data/my_test result/10.csv nn/submit/10.gbdt/20-18 gbdt
+python3 nn/test.py data/my_test result/13.csv nn/submit/13.d5_n500_soft_mlog_1_1/20-18 gbdt
+python3 nn/test.py data/my_test result/14.csv nn/submit/14.tuned_models/20-8 gbdt_wrapper
+python3 ensemble.py result/10.csv result/13.csv result/14.csv $6
--- hw2/nn/train.py
+++ hw2/nn/train.py
@@ -3,21 +3,27 @@ import sys
 import math
 import json
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
+import xgboost as xgb
+import pandas as pd
+
+from sklearn import cross_validation, metrics   
+from sklearn.grid_search import GridSearchCV
+from xgboost.sklearn import XGBClassifier
 
 from splitData import *
 
 np.set_printoptions(suppress=True)
 
-MODEL_DIR = 'models'
+
+NN_MODEL_DIR = 'models'
+SVC_MODEL_DIR = 'svc_models'
+GBDT_MODEL_DIR = 'gbdt_models'
 
 num_losses = 10
 tolerance = 0.001
 epsilon = 1e-8
 
-
-def save_model(model, config, k_fold, fold_idx, train_mean, train_std):
+def save_nn_model(model, config, k_fold, fold_idx, train_mean, train_std):
 	layers, do_dropout, drop_rate = config["nn_layers"], config["dropout"], config["drop_rate"]
 
 	model_path = ""
@@ -28,12 +34,9 @@ def save_model(model, config, k_fold, fold_idx, train_mean, train_std):
 		model_path += "_"
 
 	model_path += str(k_fold) + '-' + str(fold_idx)
-	model_path = os.path.join(MODEL_DIR, model_path)
+	model_path = os.path.join(NN_MODEL_DIR, model_path)
 	weight_path = model_path + '_weight.h5'
 	mean_std_path = model_path + '_mean_std.npy'
-
-	print(model_path, weight_path)
-	input("")
 
 	model_json = model.to_json()
 	with open(model_path, "w") as outf:
@@ -43,9 +46,12 @@ def save_model(model, config, k_fold, fold_idx, train_mean, train_std):
 	np.save(mean_std_path, [train_mean, train_std])
 
 
-def train(train_data, train_label, val_data, val_label, config, n_epoch=100, lr=1, batch_size=1, display_epoch=10, lamb=0.1, early_stop=False):
-	num_data, dim = train_data.shape
+def nn_train(train_data, train_label, val_data, val_label, config, n_epoch=100, lr=1, batch_size=1, display_epoch=10, lamb=0.1, early_stop=False, patience=5):
+	from keras.models import Sequential
+	from keras.layers import Dense, Activation, Dropout
+	from keras.callbacks import EarlyStopping
 
+	num_data, dim = train_data.shape
 	layers, do_dropout, drop_rate = config["nn_layers"], config["dropout"], config["drop_rate"]
 
 	model = Sequential()
@@ -62,23 +68,108 @@ def train(train_data, train_label, val_data, val_label, config, n_epoch=100, lr=
 
 	model.add(Dense(output_dim=2))
 	model.add(Activation('softmax'))
-
 	model.compile(	loss='categorical_crossentropy',
 					optimizer='adam',
 					metrics=['accuracy'])
 
+	early_stop = EarlyStopping(	monitor='val_loss',
+								min_delta=0,
+								patience=patience,
+								verbose=0)
+
 	if val_data is not None:
-		model.fit(train_data, train_label, batch_size=batch_size, nb_epoch=n_epoch, validation_data=(val_data, val_label))
+		model.fit(train_data, train_label, batch_size=batch_size, nb_epoch=n_epoch, callbacks=[early_stop] ,validation_data=(val_data, val_label))
 		return model, model.evaluate(val_data, val_label)
 	else:
 		model.fit(train_data, train_label, batch_size=batch_size, nb_epoch=n_epoch)
 		return model, None
 
 
+def save_svc_model(model, k_fold, fold_idx, train_mean, train_std, lamb):
+	from sklearn.externals import joblib
+
+	model_path = "svc_" + "C=" + str(lamb) + "_" + str(k_fold) + '-' + str(fold_idx)
+	model_path = os.path.join(SVC_MODEL_DIR, model_path)
+	mean_std_path = model_path + '_mean_std.npy'
+
+	# Save svc model and train mean and std
+	joblib.dump(model, model_path, compress=9)
+	np.save(mean_std_path, [train_mean, train_std])
+
+
+def svc_train(train_data, train_label, val_data, val_label, k_fold, fold_idx, train_mean, train_std, lamb=0.1, force_rebuild=False):
+	from sklearn.svm import SVC
+	from sklearn.externals import joblib
+
+	train_label = np.reshape(train_label, [-1])
+	num_data, dim = train_data.shape
+
+	model_path = "svc_" + str(k_fold) + '-' + str(fold_idx)
+	model_path = os.path.join(SVC_MODEL_DIR, model_path)
+	if os.path.isfile(model_path) and force_rebuild is False:
+		clf = joblib.load(model_path)
+	else:
+		clf = SVC(C=lamb)
+		clf.fit(train_data, train_label)
+
+
+	train_pred = clf.predict(train_data)
+	#print("Training Accuracy:" + str(np.sum(train_pred == train_label) / len(train_label)))
+
+	eval_acc = 0
+	if val_data is not None:
+		val_pred = clf.predict(val_data)
+		val_label = np.reshape(val_label, [-1])
+		eval_acc = np.sum(val_pred == val_label) / len(val_label)
+		#print("Testing Accuracy:" + str(eval_acc))
+
+	return clf, (0., eval_acc)
+
+
+def gbdt_train(train_data, train_label, val_data, val_label, tree_depth=3, num_round=400):
+
+	# Convert np array to DMatrix
+	train_label = np.reshape(train_label, [-1])
+	train_data_lbl = xgb.DMatrix(train_data, label=train_label)
+
+	if val_data is not None:
+		val_label = np.reshape(val_label, [-1])
+		val_data_lbl = xgb.DMatrix(val_data, label=val_label)
+
+
+	param = {'max_depth': tree_depth, 'eta': 0.1, 'silent': 1, 'objective': 'binary:logistic', 'subsample': 0.8, 'colsample_bytree': 0.8}
+	param['nthread'] = 4
+	param['eval_metric'] = 'error'
+
+	evallist = [(train_data_lbl, 'train')]
+	if val_data is not None:
+		evallist.append((val_data_lbl, 'eval'))
+
+	bst = xgb.train(param, train_data_lbl, num_round, evallist, early_stopping_rounds=20)
+	ypred = bst.predict(val_data_lbl)
+
+	eval_acc = None
+	if val_data is not None:
+		eval_acc = np.sum(np.around(ypred) == val_label) / len(val_label)
+
+	return bst, (0., eval_acc)
+
+
+def gbdt_save_model(model, k_fold, fold_idx, train_mean, train_std):
+
+	model_path = str(k_fold) + '-' + str(fold_idx)
+	model_path = os.path.join(GBDT_MODEL_DIR, model_path)
+	mean_std_path = model_path + '_mean_std.npy'
+
+	# Save gbdt model and train mean and std
+	model.save_model(model_path)
+	np.save(mean_std_path, [train_mean, train_std])
+
+
 def main():
 	argc = len(sys.argv)
-	if argc != 9:
-		print("Usage: python train.py X_train Y_train k_fold n_epoch lambda lr batch_size nn_config")
+	if argc != 10:
+		print("Usage: python train.py X_train Y_train k_fold n_epoch lambda lr batch_size nn_config classifier")
 		exit()
 
 	X_train = sys.argv[1]
@@ -89,6 +180,7 @@ def main():
 	lr = float(sys.argv[6])
 	batch_size = int(sys.argv[7])
 	config_path = sys.argv[8]
+	classifier = sys.argv[9]
 
 	with open(config_path, "r") as inf:
    		config = json.load(inf)	
@@ -103,26 +195,50 @@ def main():
 	else:
 		indices = [[]]
 
-	feats, data = read_data(X_train)
-	labels = read_label(Y_train, n_class=2)
+	numeric_indices, data = read_data(X_train)
+	if classifier == "nn": # or classifier == "gbdt":
+		labels = read_label(Y_train)
+	else:
+		_, labels = read_data(Y_train, dtype=np.int16)
+
+	print(numeric_indices)
+	if numeric_indices[0] != '':
+		numeric_indices = np.asarray(numeric_indices, dtype=np.uint8)
+
 
 	sum_acc, sum_err = 0, 0
 	for i in range(k_fold):
 		train_data, val_data, train_lbl, val_lbl = split_train_val(data, labels, indices[i])
 
+		train_mean = np.zeros((train_data.shape[1], ))
+		train_std = np.ones((train_data.shape[1], ))
+		if numeric_indices[0] != '':
+			train_mean[numeric_indices] = np.mean(train_data, axis=0)[numeric_indices]
+			train_std[numeric_indices] = np.std(train_data, axis=0)[numeric_indices]
+
+		'''
 		train_mean = np.mean(train_data, axis=0)
 		train_std = np.std(train_data, axis=0)
-		train_data = (train_data - train_mean) / train_std
+		'''
 
+		train_data = (train_data - train_mean) / (train_std + epsilon)
 		if val_data is not None:
-			val_data = (val_data - train_mean) / train_std
+			val_data = (val_data - train_mean) / (train_std + epsilon)
 
-		model, stat = train(train_data, train_lbl, val_data, val_lbl, config, n_epoch=n_epoch, batch_size=batch_size, lamb=lamb, early_stop=False)
-		save_model(model, config, k_fold, i, train_mean, train_std)
+		if classifier == "nn":
+			model, stat = nn_train(train_data, train_lbl, val_data, val_lbl, config, n_epoch=n_epoch, batch_size=batch_size, lamb=lamb, early_stop=False)
+			save_nn_model(model, config, k_fold, i, train_mean, train_std)
+		elif classifier == "svc":
+			model, stat = svc_train(train_data, train_lbl, val_data, val_lbl, k_fold, i, train_mean, train_std, lamb=lamb)
+			save_svc_model(model, k_fold, i, train_mean, train_std, lamb)
+		elif classifier == "gbdt":
+			model, stat = gbdt_train(train_data, train_lbl, val_data, val_lbl)
+			gbdt_save_model(model, k_fold, i, train_mean, train_std)
 
 		if stat is not None:
 			sum_acc += stat[1]
 			sum_err += stat[0]
+			print("fold %d, acc: %.4f" % (i, stat[1]))
 
 	if  k_fold > 1:
 		sys.stderr.write("Accuracy:" + str(sum_acc / k_fold) + "\n")
