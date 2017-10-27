399e7ec6ce2009d3d3ffadd55e4e14ee040034c0
Eric Lee <ericlee@Erics-MacBook-Pro.local>
2017-10-27 20:01:47 +0800

    type best -> best

--- hw2/hw2_best.sh
+++ hw2/hw2_best.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 dataProcessing.py $2 data/my_test test
-python3 best/test.py data/my_test result/10.csv bset/submit/10.gbdt/20-18 gbdt
-python3 best/test.py data/my_test result/13.csv bset/submit/13.d5_n500_soft_mlog_1_1/20-18 gbdt
-python3 best/test.py data/my_test result/14.csv bset/submit/14.tuned_models/20-8 gbdt_wrapper
+python3 best/test.py data/my_test result/10.csv best/submit/10.gbdt/20-18 gbdt
+python3 best/test.py data/my_test result/13.csv best/submit/13.d5_n500_soft_mlog_1_1/20-18 gbdt
+python3 best/test.py data/my_test result/14.csv best/submit/14.tuned_models/20-8 gbdt_wrapper
 python3 ensemble.py result/10.csv result/13.csv result/14.csv $6
