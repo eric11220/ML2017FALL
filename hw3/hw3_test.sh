7415a063b78b385b13f3fc90c22548f4a9a61c9a
Eric Lee <ericlee@Erics-MacBook-Pro.local>
2017-11-17 20:51:15 +0800

    Final commit (hopefully)

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -6,6 +6,6 @@ wget https://www.dropbox.com/s/czyeh7ddocprsyo/Model.253-0.7031-0.9231.hdf5?dl=0
 wget https://www.dropbox.com/s/xc8kdgniu5p4l6f/Model.419-0.6871-1.6949.hdf5?dl=0
 
 # Run each model
-time CUDA_VISIBLE_DEVICES="" python3 test.py $1 place_holder 0 0 Model.211-0.6870-0.9480.hdf5?dl=0 Model.253-0.7031-0.9231.hdf5?dl=0 Model.419-0.6871-1.6949.hdf5?dl=0
+python3 test.py $1 place_holder 0 0 Model.211-0.6870-0.9480.hdf5?dl=0 Model.253-0.7031-0.9231.hdf5?dl=0 Model.419-0.6871-1.6949.hdf5?dl=0
 python3 ensemble.py 1.csv 2.csv 3.csv $2
 
---
+++ hw3/hw3_train.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+python3 train.py $1 20 64 300 vgg16 categorical_crossentropy 0.2 0
