4d82aca40f34f9309d193eb5906650228386f4f8
eric11220 <b99902080@ntu.edu.tw>
2017-11-17 20:45:52 +0800

    hw3_test.sh now adapt to new test.py allowing multiple input model paths

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -6,9 +6,6 @@ wget https://www.dropbox.com/s/czyeh7ddocprsyo/Model.253-0.7031-0.9231.hdf5?dl=0
 wget https://www.dropbox.com/s/xc8kdgniu5p4l6f/Model.419-0.6871-1.6949.hdf5?dl=0
 
 # Run each model
-python3 test.py Model.211-0.6870-0.9480.hdf5 $1 1.csv 0 0
-python3 test.py Model.253-0.7031-0.9231.hdf5 $1 2.csv 0 0
-python3 test.py Model.419-0.6871-1.6949.hdf5 $1 3.csv 0 0
-
+time CUDA_VISIBLE_DEVICES="" python3 test.py $1 place_holder 0 0 Model.211-0.6870-0.9480.hdf5?dl=0 Model.253-0.7031-0.9231.hdf5?dl=0 Model.419-0.6871-1.6949.hdf5?dl=0
 python3 ensemble.py 1.csv 2.csv 3.csv $2
 
